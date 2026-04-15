fee1bc94a482dfc3b0add2978748952cbdcad240
Jesus <jesus.ecastillo25@gmail.com>
2026-04-15 18:52:07 -0300

    Agregando crearCuota a cuotasController

--- src/controllers/cuotasController.js
+++ src/controllers/cuotasController.js
@@ -1,1 +1,18 @@
 const Cuota = require("../models/Cuota");
+
+async function crearCuota(req, res) {
+  const { idSocio, monto } = req.body;
+  if (!idSocio || !monto) {
+    return res.error("Faltan campos obligatorios: idSocio y monto", 400);
+  }
+  try {
+    const nuevaCuota = await Cuota.create({ idSocio, monto });
+    res.success(nuevaCuota);
+  } catch (error) {
+    res.error(error.message, 500);
+  }
+}
+
+module.exports = {
+  crearCuota,
+};
