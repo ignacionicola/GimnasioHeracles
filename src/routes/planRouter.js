bd04eef660375f7bf5a328174a49b4956f66623e
Jesus <jesus.ecastillo25@gmail.com>
2026-05-10 00:35:35 -0300

    Añadiendo middlewares de rol y autenticacion

--- src/routes/planRouter.js
+++ src/routes/planRouter.js
@@ -1,17 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const {actualizarPlan,crearPlan,obtenerPlanes,
-validarPlan, validarActualizacionPlan
- } = require("../controllers/planController");
-
+const {
+  actualizarPlan,
+  crearPlan,
+  obtenerPlanes,
+  validarPlan,
+  validarActualizacionPlan,
+} = require("../controllers/planController");
+const authMiddleware = require("../middlewares/authMiddleware");
+const verificarAdmin = require("../middlewares/rolesMiddleware");
 // POST - Crear nuevo plan
-router.post("/admin", /* #swagger.tags=['Planes'] */ validarPlan, crearPlan);
+router.post(
+  "/admin",
+  /* #swagger.tags=['Planes'] */ authMiddleware,
+  verificarAdmin,
+  validarPlan,
+  crearPlan,
+);
 
 // GET - Obtener todos los planes
-router.get("/", /* #swagger.tags=['Planes'] */ obtenerPlanes);
+router.get("/", /* #swagger.tags=['Planes'] */ authMiddleware, obtenerPlanes);
 
 // PUT - Actualizar un plan existente
-router.put("/admin/:id", /* #swagger.tags=['Planes'] */ validarActualizacionPlan, actualizarPlan);
-
+router.put(
+  "/admin/:id",
+  /* #swagger.tags=['Planes'] */ authMiddleware,
+  verificarAdmin,
+  validarActualizacionPlan,
+  actualizarPlan,
+);
 
 module.exports = router;
